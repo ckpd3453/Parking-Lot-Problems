6d8d309993d8fa4eacd81fa5fb4b796bcd007f8b
ckpd3453 <98898121+ckpd3453@users.noreply.github.com>
2022-04-26 01:33:00 +0530

    UC3: Added Test Cases to check the availability of parking lot

--- src/test/java/com/bridgelabz/ParkingLotTest.java
+++ src/test/java/com/bridgelabz/ParkingLotTest.java
@@ -2,16 +2,15 @@ package com.bridgelabz;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ParkingLotTest {
     public static String vehicle = "Car";
     public static ParkingLot parkingLot = new ParkingLot(vehicle);
 
 
-    /*UC1: Park Vehicle
-    Test Case 1 -Able to Park The vehicle in the Parking Lot
+    /**UC1: Park Vehicle
+    Test Case 1 -Able to Park The vehicle in the Parking Lot.
      */
     @Test
     public void givenVehicle_WhenPark_ShouldReturnTrue() {
@@ -20,7 +19,7 @@ public class ParkingLotTest {
     }
 
     /*
-    Test Case 2 -Able to check weather vehicle is already parked or not
+    Test Case 2 -Able to check weather vehicle is already parked or not.
      */
     @Test
     public void givenVehicle_WhenAlreadyParked_ShouldReturnFalse() {
@@ -28,7 +27,7 @@ public class ParkingLotTest {
         assertFalse(parkingStatus);
     }
 
-    /* UC2 : Unpark Vehicle
+    /** UC2 : Unpark Vehicle
      Test Case 3 - Able to Unpark the vehicle from the parking lot.
      */
     @Test
@@ -36,4 +35,23 @@ public class ParkingLotTest {
         boolean parkingStatus = parkingLot.unParkVehicle(vehicle);
         assertTrue(parkingStatus);
     }
+
+    /**UC3 : Put out the Full Sign
+    Test Case 4 - when the parking lot is full, put out the full sign.
+     */
+    @Test
+    public void givenParkingLot_IfFull_ShouldShowFullSign() {
+        String parkingLotStatus = parkingLot.parkingLotStatus(vehicle);
+        assertEquals("Lot is Full", parkingLotStatus);
+    }
+
+    /*
+    Test Case 5 - when the parking lot is empty, show parking available.
+     */
+    @Test
+    public void givenParkingLot_IfEmpty_ShouldShowParkingAvailableSign() {
+        String parkingLotStatus = parkingLot.parkingLotStatus(null);
+        assertEquals("Parking Available", parkingLotStatus);
+    }
+
 }
